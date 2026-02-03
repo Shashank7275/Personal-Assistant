3a81026c3e750bd9deb7ec56fab83100461d1cf7
Shashank Singh <shashanks72754791@gmail.com>
2026-02-03 20:50:35 +0530

    Update Jarvis_file_opner.py

--- Jarvis_file_opner.py
+++ Jarvis_file_opner.py
@@ -1,4 +1,4 @@
-import o
+import os
 import subprocess
 import sys
 import logging
@@ -101,6 +101,7 @@ async def Play_file(name: str) -> str:
     index = await index_files(folders_to_index)
     command = name.strip()
     return await handle_command(command, index)
+
 
 
 
